75c493d7c68676924171fd71c4ea22bec7acad61
Leia <87383466+klynmc@users.noreply.github.com>
2021-10-24 20:58:32 -0500

    edit index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { writeFile, copyFile } = require('./utils/generate');
 const inquirer = require("inquirer");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 const promptUser = () => {
     return inquirer.prompt([
